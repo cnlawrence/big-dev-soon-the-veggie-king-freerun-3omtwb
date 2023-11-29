0c85e85710b670b9cd128a15a30baf8148cef1e9
cnlawrence1183 <1872389+cnlawrence@users.noreply.github.com>
2023-11-29 13:22:49 -0600

    Adding Footer component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import Navigation from "./components/Navigation.vue";
 import HomeHero from "./components/HomeHero.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
   <Navigation />
   <HomeHero />
+  <Footer />
 </template>
 
 <style scoped></style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex flex-row bg-neutral-50 drop-shadow-lg pb-10">
+    <!-- Footer Logo -->
+    <div class="flex flex-row flex-auto ml-20 mb-8 h-12">
+      <img src="../assets/icons/logo.svg" />
+    </div>
+    <!-- Footer Navigation -->
+    <div class="flex flex-row flex-auto gap-14 my-3 place-content-center">
+      <a href="#about-us" class="font-semibold">ABOUT US</a>
+      <a href="#menu" class="font-semibold">MENU</a>
+      <a href="#contact" class="font-semibold">CONTACT</a>
+    </div>
+    <!-- Footer Engaging Text and Socials -->
+    <div class="flex flex-col flex-auto mt-3">
+      <div>
+        <div>Stay up to date with our new openings, upcoming events</div>
+        <div>seasonal specials and promotions. Check our socials.</div>
+      </div>
+      <div class="flex flex-row gap-4 pt-6">
+        <img src="../assets/icons/twitter-icon.svg" alt="" />
+        <img src="../assets/icons/facebook-icon.svg" alt="" />
+        <img src="../assets/icons/instagram-icon.svg" alt="" />
+      </div>
+    </div>
+  </div>
+  <div class="ml-20 my-6">&copy; 2023 The Veggie King</div>
+</template>
+
+<style scoped></style>
